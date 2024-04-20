605f06428654e116e918d9d0b54a9181b8e2c405
ddjorlev <ddzorlev@yahoo.com>
2024-04-20 22:04:54 +0200

    recommendation done

--- recommend.py
+++ recommend.py
@@ -1,29 +1,52 @@
 from ret_data import ret_data
 from sklearn.cluster import KMeans
+import numpy as np
+
+def train_clustering_model(data):
+    # print(data)
+    ratings = []
+    for entry in data:
+        # print(entry)
+        for rating in entry[2]:
+            ratings.append(rating)
+    #ratings = [rating for entry in data for rating in entry[2]]
+
+    # Fit KMeans clustering model
+    kmeans = KMeans(n_clusters=1, random_state=42)
+    ratings = [ratings]
+
+    kmeans.fit(ratings)
+
+    return kmeans
 
 def recommend(choice, activity, day):
     data = ret_data()
+    filtered_data = [entry for entry in data if entry[1] == day or entry[1] == ""]
     
-    # Filter data based on the specified day
-    filtered_data = [entry for entry in data if entry[1] == day]
+    new_data = []
+    if activity == 0:
+        for entry in filtered_data:
+            if entry[2][0] >= 5:
+                new_data.append(entry)
+    elif activity == 1:
+        for entry in filtered_data:
+            # print(f"1: {entry[2][0]}")
+            if entry[2][0] <= 5:
+                new_data.append(entry)
+    else:
+        return "Invalid activity"
     
-    # Extract ratings for all features from the filtered data
-    ratings = [[entry[2][i] for i in range(len(choice))] for entry in filtered_data]
+    # print(new_data)
+    # Train clustering model for the chosen activity
+    new_data = np.array(new_data)
+    # new_data = new_data.reshape(-1, 1)
+    clustering_model = train_clustering_model(new_data)
 
-    # Fit KMeans clustering model
-    kmeans = KMeans(n_clusters=3, random_state=42)
-    kmeans.fit(ratings)
+    ratings = [entry[2] for entry in new_data]
+    cluster_labels = clustering_model.predict(ratings)
+    chosen_cluster = clustering_model.predict([choice])[0]
 
-    # Predict cluster labels for each event
-    cluster_labels = kmeans.predict(ratings)
-
-    # Determine which cluster center is closest to the user's choices
-    chosen_cluster = kmeans.predict([choice])[0]
-
-    # Get events belonging to the chosen cluster
-    events_in_chosen_cluster = [filtered_data[i] for i, label in enumerate(cluster_labels) if label == chosen_cluster]
-
-    # Find the event with the highest rating in the chosen cluster
+    events_in_chosen_cluster = [new_data[i] for i, label in enumerate(cluster_labels) if label == chosen_cluster]
     best_event = max(events_in_chosen_cluster, key=lambda x: max(x[2]))
 
     return best_event[0]
@@ -54,4 +77,7 @@ def recommend(choice, activity, day):
     #         best_activity = entry[0]
     #         best_rating = activity_rating
     
+choice = [0, 3, 2, 1, 2, 1]
+activity = 0
+day = "22.4.2024"
 print(recommend(choice, activity, day))
